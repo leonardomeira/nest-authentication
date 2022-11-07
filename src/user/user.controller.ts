0cf905d8a07b746bbc522f3f5aeee41b114186a7
grupocfn <informatica@casadasfechaduras.com.br>
2022-11-07 13:49:42 -0300

    (auth): implement @CurrentUser()

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
