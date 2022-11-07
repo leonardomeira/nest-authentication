77bab9df0cd5d570768ae18d8184fea1445727a2
grupocfn <informatica@casadasfechaduras.com.br>
2022-11-07 13:06:30 -0300

    (auth): implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,29 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Body, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly userService: UserService) { }
 
     async login () {
-
+        return 'Login realizado com sucesso';
     }
 
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if (user) {
+            const isPasswordValid = await bcrypt.compare(password, user.password)
+
+            if (isPasswordValid) {
+                return {
+                    ...user,
+                    password: undefined
+                };
+            }
+        }
+
+        throw new UnauthorizedException("Email ou senha incorretos")
     }
 }
