c1cda1b5303e90a8c15d60c9cb23f21f783c0fa7
grupocfn <informatica@casadasfechaduras.com.br>
2022-11-07 10:51:56 -0300

    (user): implement create

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import {
+    IsEmail,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'senha muito fraca',
+    })
+    password: string;
+
+    @IsString()
+    name: string;
+}
