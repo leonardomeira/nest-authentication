45ad7c62a4e8f5973ff8c31e9e821cc4f42dca46
grupocfn <informatica@casadasfechaduras.com.br>
2022-11-09 13:07:15 -0300

    (app): fix bug with env variables (@nestjs/config)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,22 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_GUARD,
-    useClass: JwtAuthGuard
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ],
 })
 export class AppModule {}
